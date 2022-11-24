27c61ceff19756b41063a49e682c29ba5cd4890a
EvgeniyKarpovich <karpovichevgeniy92@gmail.com>
2022-11-24 18:17:13 +0300

    create project

---
+++ src/main/java/by/karpovich/testTusk/TestTuskApplication.java
@@ -0,0 +1,13 @@
+package by.karpovich.testTusk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTuskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTuskApplication.class, args);
+
+    }
+}
